d3e884e8926d9765aed4e85579ce078625574beb
rensdimmendaal <rens@dimmendaal.com>
2020-11-16 23:23:23 +0100

    better credits

--- app/streamlit.py
+++ app/streamlit.py
@@ -19,3 +19,9 @@ user_text = st.text_input("Write here:", "trick or treat")
 output = my_module.get_emoji(user_text, model, tokenizer)
 lift = (output.sort_index() / base_output.sort_index()).sort_values(ascending=False)
 st.write("## " + emoji.emojize("".join(lift.index)))
+
+
+st.markdown(
+    "Made by [Rens](https://twitter.com/R_Dimm) with help from [VinAIResearch](https://github.com/VinAIResearch/BERTweet)"
+)
+
